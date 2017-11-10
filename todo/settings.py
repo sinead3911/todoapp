4d1ea6a0b773a68c55b9fe34e195f8401424d907
Sinead O'Neill <sinead-391@live.ie>
2017-11-10 12:45:46 +0000

    first commit

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=uxyzs9ltf#5tu!0*ae*=cqe@y^kq56%pe)_9j_=*go=i9g)#!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
