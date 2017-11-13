69f0eafa9c068ca3f153b4debfb15dc2bd5afe02
Sinead O'Neill <sinead-391@live.ie>
2017-11-13 12:20:03 +0000

    todoapp

--- todoitem/forms.py
+++ todoitem/forms.py
@@ -3,7 +3,6 @@ from .models import TodoItem
 
 class TodoItemForm(forms.ModelForm):
     class Meta:
-        model=TodoItem
-        fields= ('name', 'priority', 'done')
+        model = TodoItem
+        fields = ('name','done')
         
-    
